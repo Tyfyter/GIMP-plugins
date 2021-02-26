ed300e0aa0b08cc47d97a2da94e68f2da2ea2c46
Tyfyter <tyfyter2616@gmail.com>
2021-02-26 01:48:33 -0500

    Update EnumDiscolour.py

--- EnumDiscolour.py
+++ EnumDiscolour.py
@@ -1,44 +1,3 @@
-#!/usr/bin/env python
-#
-# -------------------------------------------------------------------------------------
-#
-# Copyright (c) 2013, Jose F. Maldonado
-# All rights reserved.
-#
-# Redistribution and use in source and binary forms, with or without modification, 
-# are permitted provided that the following conditions are met:
-#
-#	- Redistributions of source code must retain the above copyright notice, this 
-#	list of conditions and the following disclaimer.
-#	- Redistributions in binary form must reproduce the above copyright notice, 
-#	this list of conditions and the following disclaimer in the documentation and/or 
-#	other materials provided with the distribution.
-#	- Neither the name of the author nor the names of its contributors may be used 
-#	to endorse or promote products derived from this software without specific prior 
-#	written permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY 
-# EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES 
-# OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT 
-# SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, 
-# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
-# TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR 
-# BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
-# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN 
-# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH 
-# DAMAGE.
-#
-# -------------------------------------------------------------------------------------
-#
-# This file is a basic example of a Python plug-in for GIMP.
-#
-# It can be executed by selecting the menu option: 'Filters/Test/Discolour layer v1'
-# or by writing the following lines in the Python console (that can be opened with the
-# menu option 'Filters/Python-Fu/Console'):
-# >>> image = gimp.image_list()[0]
-# >>> layer = image.layers[0]
-# >>> gimp.pdb.python_fu_test_discolour_layer_v1(image, layer)
-
 import math
 import sys
 from gimpfu import *
@@ -67,10 +26,9 @@ def enum_discolor(image, drawable, expression, alpha) :
 	layer = pdb.gimp_layer_copy(drawable, false)
 	
 	newColor = (255,255,255,0)
-	# Iterate over all the pixels and convert them to gray.
+	# Iterate over all the pixels
 	try:
 		for x in range(layer.width):
-			# Update the progress bar.
 			gimp.progress_update(float(x) / float(layer.width))
 
 			for y in range(layer.height):
@@ -84,10 +42,10 @@ def enum_discolor(image, drawable, expression, alpha) :
 					global context
 					context = {'x': x, 'y': y, 'r': pixel[0], 'g': pixel[1], 'b': pixel[2], 'a': pixel[3], 'X': xyz[0], 'Y': xyz[1], 'Z': xyz[2] , 'L': LAB[0], 'A': LAB[1], 'B': LAB[2], 'C': LCh[1], 'h': LCh[2], 'width': layer.width, 'height': layer.height}
 					if(alpha):
-						newColor = eval(expression, None, context)
+						newColor = clamp_rgb(eval(expression, None, context))
 						layer.set_pixel(x,y, truncate_float4(newColor))
 					else:
-						newColor = eval(expression, None, context) + pixel[3:]
+						newColor = clamp_rgb(eval(expression, None, context)) + pixel[3:]
 						layer.set_pixel(x,y, truncate_float4(newColor))
 		
 		# Update the layer.
